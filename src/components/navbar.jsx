0a32bd6ea35277bd0a09145aeded9707c3193151
Inder Pal <inder39811@gmail.com>
2022-05-17 17:15:24 +0530

    navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+let Navbar = ()=>{
+    return (
+        <div>
+            <div>
+                <h1>LOGOBAKERY</h1>
+            </div>
+            <div>
+                <ul>
+                    <li>Services</li>
+                    <li>Projects</li>
+                    <li>About</li>
+                </ul>
+            </div>
+            <div>
+                <button>Contact</button>
+            </div>
+        </div>
+    )
+}
+export default Navbar;
